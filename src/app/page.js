04530139240c072cbae3d5a8d751f29a71086e34
Rathin Bagchi <rathinb745@gmail.com>
2025-09-26 19:08:33 +0530

    making the portfolio section

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import Footer from "@/components/Footer";
 import Services from "../components/Services";
 import TestimonialSection from "../components/TestimonialSection";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import Portfolio from "@/components/Portfolio";
 
 export default function Home() {
   return (
     <main>
       <Services />
+      <Portfolio />
        <TestimonialSection />
        <Footer/>
     </main>
